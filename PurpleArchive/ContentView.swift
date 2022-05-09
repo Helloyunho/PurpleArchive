8a17a9cae58b23128edddffa9e96137893f90606
Helloyunho <yunho050840@gmail.com>
2022-05-09 21:33:26 +0900

    🎨 chore: move the contentview to mainview

--- PurpleArchive/ContentView.swift
+++ PurpleArchive/ContentView.swift
@@ -9,37 +9,8 @@ import SwiftUI
 import DriveAPI
 
 struct ContentView: View {
-    @State var loading = true
-    @State var files = [String]()
-    @State var error: Error? = nil
-    @State var errorAlert = false
     var body: some View {
-        VStack {
-            if loading {
-                ProgressView()
-                    .progressViewStyle(.circular)
-                    .alert(Text("Error"), isPresented: $errorAlert) {
-                        Button("OK") {}
-                    } message: {
-                        Text(error?.localizedDescription ?? "")
-                    }
-            } else {
-                ForEach(files, id: \.self) { file in
-                    Text(file)
-                }
-            }
-        }
-        .frame(idealWidth: 800, maxWidth: .infinity, idealHeight: 600, maxHeight: .infinity)
-        .task {
-            do {
-                let result = try await DriveAPI.shared.getFiles("0AJEGHI5tLScTUk9PVA")
-                self.files = result.files.map({ $0.name })
-                self.loading = false
-            } catch {
-                self.error = error
-                self.errorAlert = true
-            }
-        }
+        SelectDriveView()
     }
 }
 
---
+++ PurpleArchive/Views/MainView.swift
@@ -0,0 +1,49 @@
+//
+//  MainView.swift
+//  PurpleArchive
+//
+//  Created by Helloyunho on 2022/05/09.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State var loading = true
+    @State var files = [String]()
+    @State var error: Error? = nil
+    @State var errorAlert = false
+    var body: some View {
+        VStack {
+            if loading {
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .alert(Text("Error"), isPresented: $errorAlert) {
+                        Button("OK") {}
+                    } message: {
+                        Text(error?.localizedDescription ?? "")
+                    }
+            } else {
+                ForEach(files, id: \.self) { file in
+                    Text(file)
+                }
+            }
+        }
+        .frame(idealWidth: 800, maxWidth: .infinity, idealHeight: 600, maxHeight: .infinity)
+        .task {
+            do {
+                let result = try await DriveAPI.shared.getFiles("0AJEGHI5tLScTUk9PVA")
+                self.files = result.files.map({ $0.name })
+                self.loading = false
+            } catch {
+                self.error = error
+                self.errorAlert = true
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
